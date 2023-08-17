c63203b54eeab8751a76ecfe9059909505431f56
hyunsooseol <snow@cau.ac.kr>
2023-08-17 15:11:23 +0900

    Update dataset

--- R/polytomous.b.R
+++ R/polytomous.b.R
@@ -1112,8 +1112,8 @@ adjustment; Ho= the data fit the Rasch model."
      plot <- ggplot(infit1, aes(x = item, y=infit)) + 
        geom_point(shape = 4, color = 'black', 
                   fill = 'white', size = 3, stroke = 2) +
-       geom_hline(yintercept = 1.5,linetype = "dotted", color='red', size=1.0) +
-       geom_hline(yintercept = 0.5,linetype = "dotted", color='red', size=1.0) +
+       geom_hline(yintercept = 1.5,linetype = "dotted", color='red', size=1.5) +
+       geom_hline(yintercept = 0.5,linetype = "dotted", color='red', size=1.5) +
        ggtitle("Item Infit")
      
      plot <- plot+ggtheme
@@ -1178,8 +1178,8 @@ adjustment; Ho= the data fit the Rasch model."
      plot <- ggplot(outfit1, aes(x = item, y=outfit)) + 
        geom_point(shape = 4, color = 'black', 
                   fill = 'white', size = 3, stroke = 2) +
-       geom_hline(yintercept = 1.5,linetype = "dotted", color='red', size=1.0) +
-       geom_hline(yintercept = 0.5,linetype = "dotted", color='red', size=1.0) +
+       geom_hline(yintercept = 1.5,linetype = "dotted", color='red', size=1.5) +
+       geom_hline(yintercept = 0.5,linetype = "dotted", color='red', size=1.5) +
        ggtitle("Item Outfit")
      
      plot <- plot+ggtheme
