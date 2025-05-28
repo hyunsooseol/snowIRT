cacde4b5e7d0eb405a0601c51d8f2030df8f1856
hyunsooseol <snow@cau.ac.kr>
2025-05-29 06:08:15 +0900

    del gtheory and update 2.7

--- R/00jmv.R
+++ R/00jmv.R
@@ -7,7 +7,7 @@
         `author`="Seol, H.", 
         `year`=2025, 
         `title`="snowIRT: Item Response Theory for jamovi", 
-        `publisher`="(Version 5.1.1) [jamovi module]. URL https://github.com/hyunsooseol/snowIRT", 
+        `publisher`="(Version 5.1.2) [jamovi module]. URL https://github.com/hyunsooseol/snowIRT", 
         `url`="https://github.com/hyunsooseol/snowIRT"), 
     `TAM`=list(
         `type`="software", 
